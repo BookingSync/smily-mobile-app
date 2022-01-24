4c08f1cfcb9108061c4be03633f8e3eb7d604200
EroMCakes <salah17afifi@gmail.com>
2022-01-24 15:05:05 +0100

    finalized webview with logging coockie and fixed logout callback redirection to login page

--- smily/lib/main.dart
+++ smily/lib/main.dart
@@ -1,82 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import './widgets/webview.dart';
 
-void main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
 
-  runApp(
-    EasyLocalization(
-      supportedLocales: [Locale('en', ''), Locale('fr', '')],
-      path: "Assets/Locales",
-      fallbackLocale: Locale('en', ''),
-      child: MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
       debugShowCheckedModeBanner: false,
-      title: 'Smily',
+      title: "Smily",
       theme: ThemeData(
         fontFamily: 'Opensans',
       ),
-      home: MyWebView(),
+      home: const SmilyWebview(),
     );
   }
-}
-
-class MyWebView extends StatefulWidget {
-  const MyWebView({Key? key}) : super(key: key);
-
-  @override
-  State<MyWebView> createState() => _MyWebViewState();
-}
-
-class _MyWebViewState extends State<MyWebView> {
-  late WebViewController controller;
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          toolbarHeight: 2,
-        ),
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: WebView(
-            javascriptMode: JavascriptMode.unrestricted,
-            initialUrl: 'webview_URL'.tr(),
-            onWebViewCreated: (controller) {
-              this.controller = controller;
-            },
-            javascriptChannels: Set.from([
-              JavascriptChannel(
-                  name: 'receiver',
-                  onMessageReceived: (JavascriptMessage message) {
-                    if (message == "disconnect")
-                      controller.loadUrl('webview_URL'.tr());
-                  })
-            ]),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.import_export, size: 32),
-          onPressed: () async {
-            controller.loadUrl(
-                "https://www.bookingsync.com/fr/users/login?type=smily");
-          },
-        ),
-      );
 }
---
+++ smily/lib/widgets/webview.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class SmilyWebview extends StatefulWidget {
+  const SmilyWebview({Key? key}) : super(key: key);
+
+  @override
+  _SmilyWebviewState createState() => _SmilyWebviewState();
+}
+
+class _SmilyWebviewState extends State<SmilyWebview> {
+  var webViewLink = "https://www.bookingsync.com/en/users/login?type=smily";
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+    if (Platform.localeName.toString().split("_").first == "fr") {
+      webViewLink = "https://www.bookingsync.com/fr/users/login?type=smily";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    WebViewController? controller;
+    return SafeArea(
+      child: Scaffold(
+        body: Builder(
+          builder: (BuildContext context) {
+            return WebView(
+              initialUrl: webViewLink,
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) {
+                controller = webViewController;
+              },
+              onPageStarted: (url) {
+                if (url == "https://www.bookingsync.com/fr" ||
+                    url == "https://www.bookingsync.com/en") {
+                  setState(
+                    () {
+                      controller!.loadUrl(webViewLink);
+                    },
+                  );
+                }
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
